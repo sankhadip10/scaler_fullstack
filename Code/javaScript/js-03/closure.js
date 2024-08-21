ac1a684044f2ef4b0bd67afc0b141094f7f67851
Sankhadip Das <sankhadip10.das@gmail.com>
2024-08-21 23:46:13 +0530

    JS learnings

---
+++ Code/javaScript/js-03/closure.js
@@ -0,0 +1,15 @@
+function a(){
+    let age = 24
+
+    function tellMyAge(){
+        console.log("Your age is",age)
+    }
+
+    age = 24
+    return tellMyAge
+}
+
+
+const fn = a()
+console.log(fn)
+
